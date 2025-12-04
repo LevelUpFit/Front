d20249f2391bb86e286e763478d27a83e6379675
TreeBomb-01 <htb010630@gmail.com>
2025-12-05 00:22:36 +0900

    무슨 설정파일

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cihbe3ealn"
+    "revision": "0.qi3ep9rf6ho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
