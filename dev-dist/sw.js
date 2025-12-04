f72d441b8b792e3eb6877b28c27a2d08ea7af232
TreeBomb-01 <htb010630@gmail.com>
2025-12-05 00:32:18 +0900

    피드백 메세지 박스 넘침 현상 해결
    - 피드백 메세지가 박스보다 넘치면 스크롤 활성화

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qi3ep9rf6ho"
+    "revision": "0.u3gf7pbmmv"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
